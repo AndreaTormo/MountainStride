452c4407f18c3fd4446938bd6c3df155b7150425
Andrea <Andreatormo99@gmail.com>
2026-06-02 16:54:56 +0200

    Load token system and search scripts globally

--- view/inc/bottom_page.php
+++ view/inc/bottom_page.php
@@ -1,28 +1,34 @@
 <!-- jQuery -->
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
- 
+
 <!-- Ajax Promise helper -->
 <script src="view/js/promises.js"></script>
+
+<!-- Toastr notifications -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+<!-- Token & Activity management - load on all pages -->
+<script src="module/login/view/activity_user.js"></script>
+<script src="module/login/view/middleware_auth.js"></script>
+
+<!-- Search controller - load on all pages -->
+<script src="module/search/model/controller_search.js"></script>
 
 <?php
 $page = $_GET['page'] ?? ($_GET['module'] ?? '');
 if ($page === 'controller_login') {
 ?>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script src="module/login/model/controller_login.js"></script>
 <script src="module/login/model/controller_register.js"></script>
 <?php } else { ?>
 <!-- Home controller -->
 <script src="module/home/model/controller_home.js"></script>
 
-<!-- Search controller -->
-<script src="module/search/model/controller_search.js"></script>
-  
 <!-- Shop controller -->
 <script src="module/shop/model/controller_shop.js"></script>
 <?php } ?>
- 
+
 </main>
 </body>
 </html>
